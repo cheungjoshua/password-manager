4735e14a724994c6fc82f2b8312e41bc761148c7
Joshua Cheung <contact.cjoshua@gmail.com>
2022-10-19 15:30:03 -0700

    Complete users model

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,5 +1,18 @@
 const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({});
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  user_IV: {
+    type: String,
+    require: true,
+  },
+});
 
 module.exports = mongoose.model("Users", UserSchema);
