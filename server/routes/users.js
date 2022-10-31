745da8d6be490538b6f8e47eee6d5066abe4558c
Joshua Cheung <contact.cjoshua@gmail.com>
2022-10-31 11:54:14 -0700

    succes hash password and save in DB

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
 // User Sign Up
 router.post("/signup", async (req, res) => {
   const body = req.body;
+  const salt = await bcrypt.genSalt(10);
+  hashedPassword = await bcrypt.hash(body.password, salt);
+  const user = new User({
+    username: body.username,
+    password: hashedPassword,
+  });
   try {
-    const salt = await bcrypt.genSalt(10);
-    body.password = await bcrypt.hash(body.password, salt);
+    const saveSignUpUser = await user.save();
     console.log("user sign up", body);
-    res.send(body);
+    res.send(saveSignUpUser);
   } catch (err) {
     res.status(400).json(err);
   }
