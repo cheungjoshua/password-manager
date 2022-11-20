4c8999273a139bd5e317d03339721a40b5cabbf2
Joshua Cheung <contact.cjoshua@gmail.com>
2022-11-20 15:34:07 -0800

    implenment middle ware in passwords get request

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-exports.models = function (req, res, next) {
+module.exports = function (req, res, next) {
   const token = req.header("access-token");
   if (!token) return res.status(401).send("Access Denied");
 
