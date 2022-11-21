a771bf0c6d96e44e2834812171e1a77f04a3b5f9
Joshua Cheung <contact.cjoshua@gmail.com>
2022-11-20 16:35:12 -0800

    implent decrypt function

--- server/helpers/cryptoList.js
+++ server/helpers/cryptoList.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto");
+
 // crypto algorithm
 const algorithm = "aes-256-cbc";
 
@@ -5,18 +7,33 @@ const algorithm = "aes-256-cbc";
 // It will move to env file with const secret key
 const secretKey = process.env.SECURITY_KEY;
 
-// encrypt the password list
+// encrypt individual data inside the loop
+const decryptData = (initVector, data) => {
+  const decipher = crypto.createCipheriv(algorithm, secretKey, initVector);
+  let decryptData = decipher.update(data, "hex", "utf-8");
+  decryptData += decipher.final("utf-8");
+  return decryptData;
+};
 
-const encryptList = (initVector, data) => {
-  let result = [];
+// encrypt the password list
+const decryptList = (initVector, data) => {
+  const result = data.map((dataObj) => {
+    let newData = {};
+    for (const key in dataObj) {
+      newData[key] = decryptData(initVector, dataObj.key);
+    }
+    return newData;
+  });
+
   return result;
 };
 
 // decrypt the password list
-
-const decryptList = (initVector, data) => {
-  let result = [];
-  return result;
+const encryptList = (initVector, data) => {
+  const cipher = crypto.createCipheriv(algorithm, secretKey, initVector);
+  let encryptData = cipher.update(data, "utf-8", "hex");
+  encryptData += cipher.final("hex");
+  return encryptData;
 };
 
 module.exports = { encryptList, decryptList };
