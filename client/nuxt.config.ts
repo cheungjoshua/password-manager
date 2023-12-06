6f56866b11a860bb919186b51555d0627cae9a5e
Joshua Cheung <contact.cjoshua@gmail.com>
2023-12-06 14:04:40 -0700

    fix js heap out of memory issue

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -4,8 +4,6 @@ import { resolve } from "path";
 
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  alias: {
-    "@": resolve(__dirname, "/"),
-  },
+
   css: ["~/assets/main.scss"],
 });
