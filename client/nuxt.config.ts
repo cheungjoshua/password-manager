5a476f12a5c9cb48c83afe8ddb29f0ffcb12ab40
Joshua Cheung <contact.cjoshua@gmail.com>
2023-12-08 15:41:18 -0700

    install trpc in client and server

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import { resolve } from "path";
+// import { resolve } from "path";
 
 export default defineNuxtConfig({
   devtools: { enabled: false },
-
+  // alias: {
+  //   "@": resolve(__dirname, "/"),
+  // },
   css: ["~/assets/main.scss"],
 });
