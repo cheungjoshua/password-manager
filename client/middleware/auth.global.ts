32baa68addfc601e704109c94e7d91a33b312329
Joshua Cheung <contact.cjoshua@gmail.com>
2023-12-19 16:22:54 -0700

    fix runtime config

--- client/middleware/auth.global.ts
+++ client/middleware/auth.global.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const apiUrl = useRuntimeConfig().public.api_url;
 
-export default defineNuxtRouteMiddleware((to, from) => {
-  // console.log(authCookie);
-  // if (!authCookie) {
-  //   return navigateTo("/login");
-  // }
-
-  console.log(to);
-  console.log(from);
+  const { data, error } = await useFetch(`${apiUrl}/`);
+  if (data.value) {
+    console.log("data", data.value);
+  }
+  if (error.value) {
+    console.log("error", error.value.statusCode);
+  }
 });
