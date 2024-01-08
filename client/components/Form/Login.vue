e8780dc345fa09ba95e514eff197ccfe933124c2
Joshua Cheung <contact.cjoshua@gmail.com>
2024-01-08 13:25:58 -0700

    create basic login form

--- client/components/Form/Login.vue
+++ client/components/Form/Login.vue
@@ -1,7 +1,28 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const email = ref("");
+const password = ref("");
+</script>
 <template>
   <div>
-    <p>login</p>
+    <form class="inputForm">
+      <label>Email</label>
+      <input
+        v-model="email"
+        type="text"
+        name="email"
+        placeholder="Enter Email"
+        required
+      />
+      <label>Password</label>
+      <input
+        v-model="password"
+        type="text"
+        name="password"
+        placeholder="Enter Password"
+        required
+      />
+      <button type="submit">Login</button>
+    </form>
   </div>
 </template>
 <style lang="scss" scoped></style>
