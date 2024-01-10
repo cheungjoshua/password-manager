ab7e52131b78cc0ad6ed728c83ac366ed14685c3
Joshua Cheung <contact.cjoshua@gmail.com>
2024-01-10 13:31:15 -0700

    refactor form html

--- client/components/Form/Login.vue
+++ client/components/Form/Login.vue
@@ -4,24 +4,30 @@ const password = ref("");
 </script>
 <template>
   <div class="loginFormWrapper">
+    <h2>Log In</h2>
     <form class="inputForm">
-      <label>Email</label>
-      <input
-        v-model="email"
-        type="text"
-        name="email"
-        placeholder="Enter Email"
-        required
-      />
-      <label>Password</label>
-      <input
-        v-model="password"
-        type="text"
-        name="password"
-        placeholder="Enter Password"
-        required
-      />
+      <label>
+        Email
+        <input
+          v-model="email"
+          type="text"
+          name="email"
+          placeholder="Enter Email"
+          required
+        />
+      </label>
+      <label>
+        Password
+        <input
+          v-model="password"
+          type="text"
+          name="password"
+          placeholder="Enter Password"
+          required
+        />
+      </label>
       <button type="submit">Login</button>
+      <p>Don't have account yet? <span>Create Account</span></p>
     </form>
   </div>
 </template>
--- client/components/Form/Signup.vue
+++ client/components/Form/Signup.vue
@@ -6,39 +6,49 @@ const confirmPassword = ref("");
 </script>
 <template>
   <div class="signUpFormWrapper">
+    <h2>Create Account</h2>
     <form action="">
-      <label>Username</label>
-      <input
-        v-model="username"
-        type="text"
-        name="username"
-        placeholder="Enter Username"
-        required
-      />
-      <label>Email</label>
-      <input
-        v-model="email"
-        type="text"
-        name="email"
-        placeholder="Enter Email Address"
-        required
-      />
-      <label>Password</label>
-      <input
-        v-model="password"
-        type="text"
-        name="password"
-        placeholder="Enter Password"
-        required
-      />
-      <label>Confirm Password</label>
-      <input
-        v-model="confirmPassword"
-        type="text"
-        name="confirmPassword"
-        placeholder="Enter Password"
-        required
-      />
+      <label>
+        Username
+        <input
+          v-model="username"
+          type="text"
+          name="username"
+          placeholder="Enter Username"
+          required
+        />
+      </label>
+      <label>
+        Email
+        <input
+          v-model="email"
+          type="text"
+          name="email"
+          placeholder="Enter Email Address"
+          required
+        />
+      </label>
+      <label>
+        Password
+        <input
+          v-model="password"
+          type="text"
+          name="password"
+          placeholder="Enter Password"
+          required
+        />
+      </label>
+
+      <label>
+        Confirm Password
+        <input
+          v-model="confirmPassword"
+          type="text"
+          name="confirmPassword"
+          placeholder="Enter Password"
+          required
+        />
+      </label>
       <button type="submit">Login</button>
     </form>
   </div>
