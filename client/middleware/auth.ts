84e398210d3951982af63fa658f4c8bf83683940
Joshua Cheung <contact.cjoshua@gmail.com>
2024-01-17 15:33:11 -0700

    fix bug with auth middleware

---
+++ client/middleware/auth.ts
@@ -0,0 +1,21 @@
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const apiUrl = useRuntimeConfig().public.api_url;
+
+  const { data, error } = await useFetch(`${apiUrl}/`);
+
+  if (error.value) {
+    console.log("error", error.value.statusCode);
+    return navigateTo({ path: "/sign-in" });
+  }
+
+  return;
+
+  // if (data.value) {
+  //   console.log("data", data.value);
+  //   navigateTo(to.fullPath);
+  // }
+  // if (error.value) {
+  //   console.log("error", error.value.statusCode);
+  //   return navigateTo({ path: "/sign-in" });
+  // }
+});
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: ["auth"],
+});
+</script>
+
 <template>
   <div>
     <p>Index</p>
