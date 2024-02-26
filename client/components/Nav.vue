b43a2020a7896f9e78493a408c49685fd1462c54
Joshua Cheung <contact.cjoshua@gmail.com>
2024-02-26 16:18:57 -0700

    setup dashboard body, create nav

--- client/components/Nav.vue
+++ client/components/Nav.vue
@@ -1,9 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="dashboardsWrapper">
-    <h1>It is dashboards</h1>
-  </div>
+  <div class="navWrapper"></div>
 </template>
 
 <style lang="scss" scoped></style>
