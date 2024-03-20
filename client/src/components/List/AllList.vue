5bfa10414c8864041444dc9ae0145de5ae1feb1b
Joshua Cheung <contact.cjoshua@gmail.com>
2024-03-19 18:00:35 -0600

    fix ref issue for allList

--- client/src/components/List/AllList.vue
+++ client/src/components/List/AllList.vue
@@ -1,11 +1,23 @@
 <script lang="ts" setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import axios from "axios";
 
+type PasswordType = {
+  _id: string;
+  app_name: string;
+  app_username: string;
+  app_password: string;
+};
+
+const passwordsList = ref<PasswordType[]>([]);
+
 onMounted(async () => {
-  const allPasswordsList = await axios.get("/api/passwords/", {
+  const allPasswordsListRes = await axios.get("/api/passwords/", {
     withCredentials: true,
   });
+
+  passwordsList.value = allPasswordsListRes.data;
+  console.log("the list", passwordsList.value);
 });
 </script>
 
